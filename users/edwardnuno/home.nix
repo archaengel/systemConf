1e12381f2f88fd0848d6bd48ed3b21ca476003f4
edwardnuno <god11341258@gmail.com>
2025-10-07 09:19:14 -0700

    Fix slack for wayland

--- users/edwardnuno/home.nix
+++ users/edwardnuno/home.nix
@@ -66,7 +66,48 @@ in
       pavucontrol
       playerctl
       ripgrep
-      slack
+      (slack.overrideAttrs (oldAttrs: {
+        # Upstream slack doesn't appear to respect ozone-platform-hint, but ozone-platform=wayland explicitly works
+        installPhase = ''
+          runHook preInstall
+
+          # The deb file contains a setuid binary, so 'dpkg -x' doesn't work here
+          dpkg --fsys-tarfile $src | tar --extract
+          rm -rf usr/share/lintian
+
+          mkdir -p $out
+          mv usr/* $out
+
+          # Otherwise it looks "suspicious"
+          chmod -R g-w $out
+
+          for file in $(find $out -type f \( -perm /0111 -o -name \*.so\* \) ); do
+            patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" "$file" || true
+            patchelf --set-rpath ${oldAttrs.rpath}:$out/lib/slack $file || true
+          done
+
+          # Replace the broken bin/slack symlink with a startup wrapper.
+          # Make xdg-open overrideable at runtime.
+          rm $out/bin/slack
+          makeWrapper $out/lib/slack/slack $out/bin/slack \
+            --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
+            --suffix PATH : ${lib.makeBinPath [ pkgs.xdg-utils ]} \
+            --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland --enable-features=WaylandWindowDecorations,WebRTCPipeWireCapturer --enable-wayland-ime=true}}"
+
+          # Fix the desktop link
+          substituteInPlace $out/share/applications/slack.desktop \
+            --replace /usr/bin/ $out/bin/ \
+            --replace /usr/share/pixmaps/slack.png slack \
+            --replace bin/slack "bin/slack -s"
+
+          # Prevent Un-blacklist pipewire integration to enable screen sharing on wayland.
+          # https://github.com/flathub/com.slack.Slack/issues/101#issuecomment-1807073763
+          sed -i -e 's/,"WebRTCPipeWireCapturer"/,"LebRTCPipeWireCapturer"/' $out/lib/slack/resources/app.asar
+
+          runHook postInstall
+        '';
+
+      }))
       spotify
       swappy
       terraform
@@ -97,6 +138,11 @@ in
       env=TERMCMD=ghostty -e
     '';
   };
+
+  xdg.configFile."electron-flags.conf".text = ''
+    --enable-features=UseOzonePlatform
+    --ozone-platform=wayland
+  '';
 
   programs.yazi = {
     enable = true;
